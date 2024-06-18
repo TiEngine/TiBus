1dfdcb0f664529bc1408ca0c93ebe098c145d27b
Deu.K <370242479@qq.com>
2024-06-19 00:18:48 +0800

    script: 支持通过build.sh指定编译Debug或Release模式

--- build.sh
+++ build.sh
@@ -4,6 +4,37 @@ set -e
 CURRENT_PATH=$(cd "$(dirname "$0")"; pwd)
 cd ${CURRENT_PATH}
 
+usage() {
+    echo ""
+    echo "Usage: bash <workspace>/build.sh [options]"
+    echo "-b | --build_config_mode, { Debug, Release }"
+    echo "-h | --help"
+    echo ""
+}
+
+parse_args() {
+    for arg in "$@"
+    do
+        case $arg in
+            -h | --help)
+            usage
+            exit
+            ;;
+            -b=* | --build_config_mode=*)
+            build_config_mode="${arg#*=}"
+            ;;
+            *)
+            echo "Error: unknown parameter $arg"
+            usage
+            exit 1
+            ;;
+        esac
+    done
+}
+
+build_config_mode=Debug
+parse_args $@
+
 cmake -S . -B build
-cmake --build build --config Release --parallel
-cmake --install build --prefix output --config Release
+cmake --build build --config ${build_config_mode} --parallel
+cmake --install build --prefix output --config ${build_config_mode}
